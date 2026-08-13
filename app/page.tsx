cadc7ad8696f4504ace8ed0e244572cd7dc6873f
Reeshav <reeshav.nero@gmail.com>
2026-08-14 00:20:46 +0530

    basic change

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import EatMeButtonPreview from "@/components/previews/eat-me-button-preview";
 import HeroSection from "@/components/ui/HeroSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      <EatMeButtonPreview />
+
     </>
   );
 }
